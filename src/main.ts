be5ba5bd4a394cd2305afcba8a017905956307e9
leví <20235838@esen.edu.sv>
2026-03-05 21:41:48 -0600

    Integración de Swagger UI, habilitación de CORS y modificación de README

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Eventos Empresariales - Grupo Theta')
+    .setDescription('Documentación de los endpoints para la integración con el Front-End')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
