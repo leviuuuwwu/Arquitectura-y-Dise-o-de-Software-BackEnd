1360e7fc4f4c600c36969afa5fcc7e5674565d91
leví <20235838@esen.edu.sv>
2026-03-05 21:33:41 -0600

    Configuración inicial de Docker, TypeORM y dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolesModule } from './modules/roles/roles.module';
+import { CategoriaEventoModule } from './modules/categoria-evento/categoria-evento.module';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { EmpresaModule } from './modules/empresa/empresa.module';
+import { EventoModule } from './modules/evento/evento.module';
+import { RegistroAsistenciaModule } from './modules/registro-asistencia/registro-asistencia.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'suser', 
+      database: 'ProyectoCatedra',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    RolesModule,
+    CategoriaEventoModule,
+    UsuarioModule,
+    EmpresaModule,
+    EventoModule,
+    RegistroAsistenciaModule,
+  ],
 })
 export class AppModule {}
