15b382fe45656e89abfbe4f77cdb70641b69ddc0
leví <20235838@esen.edu.sv>
2026-03-14 21:20:40 -0600

    bcrypt y crud

--- src/modules/usuario/controllers/usuario.controller.ts
+++ src/modules/usuario/controllers/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, HttpCode, HttpStatus, UnauthorizedException } from '@nestjs/common';
 import { UsuarioService } from '../use-cases/usuario.service';
 import { Usuario } from '../entities/usuario.entity';
 
@@ -14,5 +14,33 @@ export class UsuarioController {
   @Post()
   create(@Body() usuario: Usuario) {
     return this.usuarioService.create(usuario);
+  }
+
+  // Endpoint de Login
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() body: any) {
+    const user = await this.usuarioService.login(body.user_email, body.user_password);
+    
+    if (!user) {
+      throw new UnauthorizedException('Correo o contraseña incorrectos');
+    }
+    
+    return user;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usuarioService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateData: Partial<Usuario>) {
+    return this.usuarioService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usuarioService.remove(id);
   }
 }
--- src/modules/usuario/use-cases/usuario.service.ts
+++ src/modules/usuario/use-cases/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Usuario } from '../entities/usuario.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsuarioService {
@@ -16,5 +17,39 @@ export class UsuarioService {
 
   create(usuario: Usuario): Promise<Usuario> {
     return this.usuarioRepository.save(usuario);
+  }
+
+  
+  async login(email: string, pass: string): Promise<any> {
+    const user = await this.usuarioRepository.findOne({ 
+      where: { user_email: email }, 
+      relations: ['role'] 
+    });
+
+    if (user && await bcrypt.compare(pass, user.user_password)) {
+      const { user_password, ...result } = user;
+      return result; 
+    }
+    return null; 
+  }
+
+  async findOne(id: string): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOne({ 
+      where: { user_id: id }, 
+      relations: ['role'] 
+    });
+    if (!usuario) {
+      throw new Error(`Usuario with id ${id} not found`);
+    }
+    return usuario;
+  }
+
+  async update(id: string, updateData: Partial<Usuario>): Promise<Usuario> {
+    await this.usuarioRepository.update(id, updateData);
+    return this.findOne(id); 
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.usuarioRepository.delete(id);
   }
 }
